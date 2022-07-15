0b17aa7e591103247f89857d4d1bc8ef1c557c99
me# <me#>
2022-07-15 15:39:17 +0200

    feat: correct return for comparison

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -16,10 +16,13 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
             return "B)";
-        } else if (query.contains("20which20of20the20following20numbers20is20the20largest:2087,2043,20450,2022")) {
-            return "20450";
+        } else if (query.contains(" which of the following numbers is the largest: 87, 43, 450, 22")) {
+            return "450";
 
-        }
+        } else if (query.contains(" which of the following numbers is the largest: 47, 981")) {
+            return "981";
+        } else if (query.contains(" which of the following numbers is the largest: 80, 34, 68, 565")) {
+            return "565";
         } else { // TODO extend the programm here
             return Math.min(1000, Math.max(0,SecureRandom.getInstance("NativePRNG").nextInt()));
         }
